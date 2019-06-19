8281ecc2e5f0be3ef115c2e6d850705fc939e7ee
Computer <gabecadiz@hotmail.com>
2019-06-18 23:29:14 -0400

    added validation to register and signin, form cannot have empty inputs

---  controllers/register.js
+++  controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegisterPOST = (req, res, db, bcrypt, saltRounds) => {
   const { name, email, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
   var hash = bcrypt.hashSync(password, saltRounds);
   db.transaction(trx => {
     trx

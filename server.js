e3c2c6adb4c3a070b56629e817057587c2e07e89
Computer <gabecadiz@hotmail.com>
2019-06-18 22:47:00 -0400

    created controllers folder, created register controller, more to be implemented

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const saltRounds = 10;
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./ controllers/register');
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -53,33 +55,7 @@ app.post('/signin', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  var hash = bcrypt.hashSync(password, saltRounds);
-  db.transaction(trx => {
-    trx
-      .insert({
-        email: email,
-        hash: hash
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            name: name,
-            email: loginEmail[0],
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => {
-    res.status(400).json('unable to register');
-  });
+  register.handleRegister(req, res, db, bcrypt, saltRounds);
 });
 
 app.get('/profile/:id', (req, res) => {
